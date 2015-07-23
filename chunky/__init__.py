2b3faa142fe7daedceb52a315f76e061f6568f2f
Kapil Ratnani <kapil.ratnani@myntra.com>
2015-07-24 00:52:13 +0530

    making one single class to represent a chunked file

--- chunky/__init__.py
+++ chunky/__init__.py
@@ -1,53 +1,64 @@
 # -*- coding:utf-8 -*-
 """
 Chunky
-A utility class to read and write files in chunks.
+A utility to read and write files in chunks.
 
-Main entry-point for chunky
+Inspired from gzip.py in python 2.7 standard library.
 """
+import io
 import os
 import string
 from __builtin__ import open as py_open
 
-__all__ = ["ChunkReader", "ChunkWriter", "open"]
+__author__ = ('Kapil Ratnani', 'kapil.ratnani@iiitb.net')
+
+__all__ = ["ChunkedTextFile", "open"]
 
 
-class ChunkReader:
-    def __init__(self, file_pattern):
-        pass
+class ChunkedTextFile(io.TextIOBase):
+    """ChunkedTextFile
+        A class for reading and writing files which are split into
+        number of files of fixed chunk size; Chunk size is number of
+        lines. This gives a familiar abstraction of interacting with
+        files without dealing with
 
-    def readline(self, limit=-1):
-        pass
+        In write mode, on each call to write() it records the number of
+        lines written (by counting newline characters) and when the
+        number of lines reaches the specified chunk size, a new file
+        is created and writing continues in the new file.
 
-    def close(self):
-        pass
+        In read mode, it starts reading from the first file till EOF
+        and continues to other file, if any.
 
-    def __repr__(self):
-        return "<ChunkReader %s>" % self.file_pattern
-
-
-class ChunkWriter:
-    """ChunkWriter writes files in sizes of chunk_size each.
-    Creates files as per the pattern specified.
-    Only works for text files.
+        Files are created/read based on the pattern specified.
+        In write mode, increments the number placeholder each time the
+        line count reaches the chunk size.
+        In read mode, it reads each file which satisfies the pattern
+        till EOF, and stops when no files are found having the pattern.
+        The reading is in the order of creation.
     """
 
-    def __init__(self, file_pattern, chunk_size,
-                 cb_chunk_written, cb_chunk_start):
+    def __init__(self, mode, file_pattern, chunk_size,
+                 cb_chunk_closed, cb_chunk_start):
         """Constructor for the ChunkWriter class
 
-        :param file_pattern: pattern which specifies the names of files to be created
+        :param file_pattern: pattern which specifies the names of
+        files to be created.
+        Example: /tmp/test_20150723_{0}.txt . This will generate
+        file names as
+        /tmp/test_20150723_0.txt, /tmp/test_20150723_1.txt ...
         :param chunk_size: maximum number of lines in a chunk
-        :param cb_chunk_written: callback for notifying end of a chunk
+        :param cb_chunk_closed: callback for notifying end of a chunk
         :param cb_chunk_start: callback for notifying start of a chunk
         """
+        self.mode = mode
         self.pattern = file_pattern
         self.__check_pattern()
         self.chunk_size = chunk_size
         self.cnum = 0
         self.line_count = 0
         self.fileobj = None
-        self.cb_chunk_written = cb_chunk_written
+        self.cb_chunk_closed = cb_chunk_closed
         self.cb_chunk_start = cb_chunk_start
         self.__make_new_file()
 
@@ -81,22 +92,25 @@ class ChunkWriter:
         if num_replacement_field > 1:
             raise ValueError("Only 1 replacement field is supported")
 
-    def __make_new_file(self):
+    def __make_next_filename(self):
         self.current_file = self.pattern.format(self.cnum)
+        self.cnum += 1
+
+    def __make_new_file(self):
+        self.__make_next_filename()
         self.fileobj = py_open(self.current_file, 'w')
         self.__notify_chunk_start()
-        self.cnum += 1
 
     def __close_current(self):
         if self.fileobj:
             self.fileobj.close()
-            self.__notify_chunk_written()
+            self.__notify_chunk_closed()
             self.fileobj = None
 
-    def __notify_chunk_written(self):
-        if self.cb_chunk_written:
-            if hasattr(self.cb_chunk_written, '__call__'):
-                self.cb_chunk_written(self.current_file, self.line_count)
+    def __notify_chunk_closed(self):
+        if self.cb_chunk_closed:
+            if hasattr(self.cb_chunk_closed, '__call__'):
+                self.cb_chunk_closed(self.current_file, self.line_count)
             else:
                 raise ValueError("callback is not callable.")
 
@@ -109,12 +123,28 @@ class ChunkWriter:
 
     def write(self, s):
         self.__check_closed()
+        self.__check_writable()
         if self.line_count == self.chunk_size:
             self.__close_current()
             self.line_count = 0
             self.__make_new_file()
         self.fileobj.write(s)
         self.line_count += s.count(os.linesep)
+
+    def __check_writable(self):
+        pass
+
+    def readline(self):
+        pass
+
+    def readable(self):
+        return self.mode == 'r'
+
+    def writable(self):
+        return self.mode == 'w'
+
+    def seekable(self):
+        return False
 
     def __check_closed(self):
         if self.fileobj is None:
@@ -124,23 +154,23 @@ class ChunkWriter:
         self.__close_current()
 
     def __repr__(self):
-        return "<ChunkWriter %s>" % self.pattern
+        return "<ChunkedTextFile %s>" % self.pattern
 
 
 def open(file_pattern, mode='r', chunk_size=1000,
-         cb_chunk_written=None, cb_chunk_start=None):
+         cb_chunk_closed=None, cb_chunk_start=None):
     """
     :param file_pattern: filename with a placeholder({0}) for numbering chunks
     :param mode: 'r' for read, 'w' for write
     :param chunk_size: Only for 'w' mode. This will create files containing chunks of specified number of lines
-    :param cb_chunk_written: only for 'w' mode. Callback to be called when a chunk is written and closed.
+    :param cb_chunk_closed: Callback to be called when a chunk is closed.
+     In write mode, when the chunk size is reached and the file is closed.
+     In read mode, when the the current chunk is read till EOF.
     :param cb_chunk_start: Callback to be called when a new chunk starts
-    :returns: for 'r' ChunkReader and for 'w' ChunkWriter
+    :returns: ChunkedTextFile
     """
-    if mode == 'r':
-        return ChunkReader(file_pattern)
-    elif mode == 'w':
-        return ChunkWriter(file_pattern, chunk_size,
-                           cb_chunk_written, cb_chunk_start)
+    if mode == 'w' or mode == 'r':
+        return ChunkedTextFile(mode, file_pattern, chunk_size,
+                               cb_chunk_closed, cb_chunk_start)
     else:
         raise ValueError("Not a supported mode.")
--- tests/test_chunky.py
+++ tests/test_chunky.py
@@ -9,11 +9,11 @@ import chunky
 
 def test_chunky_open():
     reader = chunky.open("/tmp/test_{0}.txt", 'r')
-    assert isinstance(reader, chunky.ChunkReader)
+    assert isinstance(reader, chunky.ChunkedTextFile)
     reader.close()
 
     writer = chunky.open("/tmp/test_{0}.txt", 'w')
-    assert isinstance(writer, chunky.ChunkWriter)
+    assert isinstance(writer, chunky.ChunkedTextFile)
     writer.close()
 
     # test unsupported mode
@@ -30,7 +30,7 @@ def file_get_contents(path):
 
 def test_chunkwriter_onefile():
     writer = chunky.open("/tmp/test_{0}.txt", 'w', 10)
-    assert isinstance(writer, chunky.ChunkWriter)
+    assert isinstance(writer, chunky.ChunkedTextFile)
 
     for i in range(0, 10):
         writer.write(str(i) + os.linesep)
@@ -57,7 +57,7 @@ def test_chunkwriter_onefile():
 
 def test_chunkwriter_multiple_files():
     writer = chunky.open("/tmp/test_{0}.txt", 'w', 10)
-    assert isinstance(writer, chunky.ChunkWriter)
+    assert isinstance(writer, chunky.ChunkedTextFile)
 
     for i in range(0, 16):
         writer.write(str(i) + os.linesep)
@@ -118,7 +118,7 @@ def test_chunkwriter_callback():
 
     writer = chunky.open(
         "/tmp/test_{0}.txt", 'w', chunk_size=10,
-        cb_chunk_written=callback)
+        cb_chunk_closed=callback)
 
     for i in range(0, 35):
         writer.write(str(i) + os.linesep)
@@ -134,7 +134,7 @@ def test_chunkwriter_callback():
 
 
 def test_chunkwriter_callback_error():
-    writer = chunky.open("/tmp/test_{0}.txt", 'w', cb_chunk_written="NotACallback")
+    writer = chunky.open("/tmp/test_{0}.txt", 'w', cb_chunk_closed="NotACallback")
 
     for i in range(0, 5):
         writer.write(str(i) + os.linesep)
@@ -178,7 +178,7 @@ def test_with_csv_dictwriter():
         assert num_lines_written > 0
 
     csvfile.cb_chunk_start = chunk_start
-    csvfile.cb_chunk_written = chunk_written
+    csvfile.cb_chunk_closed = chunk_written
 
     writer.writeheader()
     for i in range(0, 16):
