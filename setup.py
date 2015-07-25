ce16acc8dd69194f08bdee3e2325b387ada298ff
Kapil Ratnani <kapil.ratnani@myntra.com>
2015-07-25 11:42:12 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'A python library to handle reading and writing of text files in chunks.',
+    'description': 'A python module to handle reading and writing of text files in chunks.',
     'author': 'Kapil Ratnani',
     'url': 'http://github.com/kapilratnani/chunky',
-    'download_url': 'http://github.com/kapilratnani/chunky',
+    'download_url': 'http://github.com/kapilratnani/chunky/tarball/0.5',
     'author_email': 'kapil.ratnani@iiitb.net',
-    'version': '0.1',
+    'version': '0.5',
     'install_requires': [],
+    'keywords':['chunk', 'parts', 'files', 'chunks', 'split'],
     'packages': ['chunky'],
     'name': 'chunky'
 }
